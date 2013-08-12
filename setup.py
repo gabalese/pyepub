0ed2f203fe9b257dc2b25b2f229a6c333ca4d53d
gabalese <gabriele@alese.it>
2013-08-12 19:57:17 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pyepub',
+    version='0.1.0',
+    packages=[''],
+    url='http://www.alese.it/pyepub',
+    license='MIT',
+    author='Gabriele Alese',
+    author_email='gabriele@alese.it',
+    description='Enhanced EPUB libraary',
+    long_description=open("README.md", "r").read()
+)
