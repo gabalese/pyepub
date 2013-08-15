99e5aac2133b7bb581cc805a35a880e18c235b66
gabalese <gabriele@alese.it>
2013-08-15 15:25:05 +0200

    Bugfix

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pyepub',
-    version='0.1.0',
-    packages=[''],
-    url='http://www.alese.it/pyepub',
+    version='0.1.1',
+    packages=['pyepub'],
+    url='http://blog.alese.it/pyepub',
     license='MIT',
     author='Gabriele Alese',
     author_email='gabriele@alese.it',
-    description='Enhanced EPUB libraary',
-    long_description=open("README.md", "r").read()
+    description='Enhanced EPUB library'
 )
