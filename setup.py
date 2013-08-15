dde41f25444b5f4a42a65976d70eaeff53209de6
gabalese <gabriele@alese.it>
2013-08-15 16:35:26 +0200

    Performance enhancement, added support for lxml, if present

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyepub',
-    version='0.1.2',
+    version='0.2.0',
     packages=['pyepub'],
     url='http://blog.alese.it/pyepub',
     license='MIT',
