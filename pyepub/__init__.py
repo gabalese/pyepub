01a29a5327c8812ecc7fc3e01ea6c189a8470e79
gabalese <gabriele@alese.it>
2013-08-17 22:00:28 +0200

    Bugfix with problematic but readable zipfiles

--- pyepub/__init__.py
+++ pyepub/__init__.py
@@ -298,9 +298,12 @@ class EPUB(zipfile.ZipFile):
         new_zip = zipfile.ZipFile(FLO, 'w')
         for item in self.infolist():
             if item.filename not in paths:
-                new_zip.writestr(item.filename, self.read(item.filename))
+                try:
+                    new_zip.writestr(item.filename, self.read(item.filename))
+                except zipfile.BadZipfile:
+                    pass
         zipfile.ZipFile.close(self)     # Don't know why
-        new_zip.close()             # but it works, don't ever touch
+        new_zip.close()                 # but it works, don't ever touch
         zipfile.ZipFile.__init__(self, FLO, mode="a")
 
     def additem(self, fileObject, href, mediatype):
